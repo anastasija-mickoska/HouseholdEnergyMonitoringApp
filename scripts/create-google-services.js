f284d9e3468daad628f492651ebc03051455963a
Anastasija <Anastasija@DESKTOP-6K6QS02>
2025-07-28 01:43:03 +0200

    change

--- scripts/create-google-services.js
+++ scripts/create-google-services.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const source = path.join(__dirname, '..', 'secrets', 'google-services.json');
+const source = path.join(__dirname, 'secrets', 'google-services.json');
 const dest = path.join(__dirname, '..', 'android', 'app', 'google-services.json');
 
 if (!fs.existsSync(source)) {
