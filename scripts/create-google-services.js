65ca4535c3f938295e22c0ea00069f73f8dd5f26
Anastasija <Anastasija@DESKTOP-6K6QS02>
2025-08-07 13:58:55 +0200

    new build changes

--- scripts/create-google-services.js
+++ scripts/create-google-services.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const source = path.join(__dirname, 'secrets', 'google-services.json');
-const dest = path.join(__dirname, '..', 'android', 'app', 'google-services.json');
+const googleServicesBase64 = process.env.GOOGLE_SERVICES_JSON;
 
-if (!fs.existsSync(source)) {
-  console.error('google-services.json not found in secrets folder.');
+if (!googleServicesBase64) {
+  console.error('Missing GOOGLE_SERVICES_JSON env variable');
   process.exit(1);
 }
 
-fs.copyFileSync(source, dest);
-console.log('Copied google-services.json to android/app folder');
+const decoded = Buffer.from(googleServicesBase64, 'base64').toString('utf-8');
+
+const targetPath = path.join(__dirname, '..', 'android', 'app', 'google-services.json');
+
+fs.writeFileSync(targetPath, decoded);
+
+console.log('google-services.json created');
